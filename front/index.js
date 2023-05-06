2782874ce1fb2ebb2d33a078c770a45447980ff0
Jay Patel <74251170+AlphaWo1f@users.noreply.github.com>
2023-05-06 13:38:33 -0400

    Added icons
    Co-Authored-By: Mellostar25 <67835601+Mellostar25@users.noreply.github.com>
    Co-Authored-By: Christian <60714838+Shadow2ube@users.noreply.github.com>
    Co-Authored-By: Justin Dhillon <83477376+JustinPrograms@users.noreply.github.com>

--- front/index.js
+++ front/index.js
@@ -1,9 +1,13 @@
+const { json } = require("express/lib/response");
 
 
+// Api key for mapbox 
 const apiKey = 'pk.eyJ1IjoianA4NDEzMzEiLCJhIjoiY2wzZjEwMGE0MDBiaTNla2JsZnB0M3RmdSJ9.ZLQyQW6BpT7i2PcIG2beOw';
 
-
+// Initializing variables
+    let pos;
     const status = document.querySelector('.status');
+// Lamada 
     navigator.geolocation.getCurrentPosition((position) => {
         console.log(position);
         pos = [position.coords.latitude, position.coords.longitude];
@@ -18,7 +22,88 @@ const apiKey = 'pk.eyJ1IjoianA4NDEzMzEiLCJhIjoiY2wzZjEwMGE0MDBiaTNla2JsZnB0M3Rmd
     accessToken: apiKey
 }).addTo(mymap);
 
-const marker = L.marker([pos[0], pos[1]]).addTo(mymap);
 
+//initializing Icons
+var userIcon = L.icon({
+  iconUrl: 'userIcon.png',
+
+  iconSize:     [38, 95], // size of the icon
+  iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+
+
+
+const marker = L.marker([pos[0], pos[1]], {icon: userIcon }).addTo(mymap);
+return pos;
     })
+
+console.log("hello")
+console.log(pos)
+function getLocation () {
+    console.log(navigator.geolocation.getCurrentPosition(showPosition)) 
+}
  
+function showPosition (position) {
+    console.log("Latitude  " + position.coords.latitude)
+    console.log("Longitude " + position.coords.longitude)
+
+
+    var foodIcon = L.icon({
+        iconUrl: 'foodIcon.png',
+      
+        iconSize:     [38, 95], // size of the icon
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+      });
+    
+      var shelterIcon = L.icon({
+        iconUrl: 'shelterIcon.png',
+      
+        iconSize:     [38, 95], // size of the icon
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+      });
+    
+      var waterIcon = L.icon({
+        iconUrl: 'waterIcon.png',
+      
+        iconSize:     [38, 95], // size of the icon
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+      });
+    
+
+
+    //if user chooses food, shelter, water:
+    var water = document.getElementById('water').checked;
+    var food = document.getElementById('food').checked;
+    var civilization = document.getElementById('civilization').checked
+    var essentialName = document.getElementById('name').value;
+
+    if(water == true){
+        const marker = L.marker([position.coords.latitude, position.coords.longitude], {icon: waterIcon }).addTo(mymap);
+    }else if(food == true){
+        const marker = L.marker([position.coords.latitude, position.coords.longitude], {icon: foodIcon }).addTo(mymap);
+    }else if(civilization == true){
+        const marker = L.marker([position.coords.latitude, position.coords.longitude], {icon: shelterIcon }).addTo(mymap);
+    }
+
+
+    fetch(
+        "https://api.unmined.ca",
+        {
+            "body": {
+                "lat": position.coords.latitude,
+                "lon": position.coords.longitude,
+                "dist": 2
+            }
+        }
+    ).then(res => JSON.parse(res))
+    .then(data => console.log(data));
+
+
+    //send to backend
+    
+  }
